afd8e1b4850588a44d32b63f136dd726d8e9c1db
Alejandro Rivas <jarlalejor@>
2019-12-31 09:42:37 -0500

    add babel

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-const http = require('http')
-const mongoose = require('mongoose')
+import http from 'http'
+import mongoose from 'mongoose'
+import socketIO from 'socket.io'
 
-const config = require('./config')
-const Tasks = require('./Tasks')
+import config from './config'
+import Tasks from './Tasks'
 
-const PORT = process.env.PORT || 6005
-const server = http.createServer((req, res) => res.end('WebSocket'))
+const PORT = process.env.PORT || 6002
+const server = http.createServer((req, res) => {
+	res.writeHead(200, { 'Content-Type': 'text/plain' })
+	res.end('WebSocket')
+})
 
-const io = require('socket.io')(server)
-
+const io = socketIO(server)
 io.sockets.on('connection', socket => {
 
 	socket.on('created::task', async data => {
@@ -17,14 +20,13 @@ io.sockets.on('connection', socket => {
 	})
 })
 
-
 mongoose.Promise = global.Promise
 mongoose.set('useUnifiedTopology', true)
 
 mongoose.connect(config.database.uri, { useNewUrlParser: true })
-.then(() => {
-	server.listen(PORT, () => {
-		console.log('Conectado a MongoDB..')
-		console.log(`Sever running in https://localhost:${PORT}`)
+	.then(() => {
+		server.listen(PORT, () => {
+			console.log('Conectado a MongoDB..')
+			console.log(`Sever running in https://localhost:${PORT}`)
+		})
 	})
-})
